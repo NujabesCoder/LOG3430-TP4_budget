9b237654ecb29e47d64d71e7e1d7edc6927e136a
Diarse <cool.maguin2010@yandex.ru>
2024-11-02 19:03:44 -0400

    Ajout de nouveau script et modification de main

--- myscript.py
+++ myscript.py
@@ -1,17 +1,4 @@
 import os
-import sys
 
-# Obtenir les hachages des commits "mauvais" et "bon"
-badhash = os.getenv("BAD_HASH", "c1a4be04b972b6c17db242fc37752ad517c29402")  # exemple de hash mauvais
-goodhash = os.getenv("GOOD_HASH", "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")  # exemple de hash bon
-
-# Fonction principale pour exécuter git bisect
-def run_bisect():
-    os.system(f"git bisect start {badhash} {goodhash}")
-    # Utilisation de pytest pour exécuter les tests à chaque étape de bisect
-    result = os.system("git bisect run pytest")
-    os.system("git bisect reset")
-    sys.exit(result)
-
-if __name__ == "__main__":
-    run_bisect()
+os.system("git bisect start $badhash $goodhash")
+os.system("git bisect run python managa.py test")
